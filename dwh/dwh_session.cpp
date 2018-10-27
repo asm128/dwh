b7e77150970d9bee84d691b27a6a96180eb46a87
asm128 <pabloarielz@outlook.com>
2018-10-27 02:45:52 -0300

    -

---
+++ dwh/dwh_session.cpp
@@ -0,0 +1,6 @@
+#include "dwh_session.h"
+
+
+	::gpk::error_t					dwh::sessionClientStart				(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionClientLogin				(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionClientClose				(::gpk::array_pod<byte_t> & output) { output; return 0; }
---
+++ dwh/dwh_session.h
@@ -0,0 +1,16 @@
+#include "gpk_array.h"
+
+#ifndef DWH_SESSION_H_2987492834
+#define DWH_SESSION_H_2987492834
+
+namespace dwh
+{
+	struct SSessionClient {};
+	struct SSessionServer { ::gpk::array_pod<SSessionClient> Clients; };
+
+	::gpk::error_t							sessionClientStart							(::gpk::array_pod<byte_t> & output);
+	::gpk::error_t							sessionClientLogin							(::gpk::array_pod<byte_t> & output);
+	::gpk::error_t							sessionClientClose							(::gpk::array_pod<byte_t> & output);
+}
+
+#endif // DWH_SESSION_H_2987492834
