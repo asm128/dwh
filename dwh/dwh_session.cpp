b46c7a172a27a8598074a5957f751849bd6384da
asm128 <pabloarielz@outlook.com>
2018-10-29 08:38:03 -0300

    -

--- dwh/dwh_session.cpp
+++ dwh/dwh_session.cpp
@@ -1,6 +1,10 @@
 #include "dwh_session.h"
 
 
-	::gpk::error_t					dwh::sessionClientStart				(::gpk::array_pod<byte_t> & output) { output; return 0; }
-	::gpk::error_t					dwh::sessionClientLogin				(::gpk::array_pod<byte_t> & output) { output; return 0; }
-	::gpk::error_t					dwh::sessionClientClose				(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionClientStart								(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionClientLogin								(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionClientClose								(::gpk::array_pod<byte_t> & output) { output; return 0; }
+
+	::gpk::error_t					dwh::sessionServerClientStart						(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionServerClientLogin						(::gpk::array_pod<byte_t> & output) { output; return 0; }
+	::gpk::error_t					dwh::sessionServerClientClose						(::gpk::array_pod<byte_t> & output) { output; return 0; }
--- dwh/dwh_session.h
+++ dwh/dwh_session.h
@@ -11,6 +11,10 @@ namespace dwh
 	::gpk::error_t							sessionClientStart							(::gpk::array_pod<byte_t> & output);
 	::gpk::error_t							sessionClientLogin							(::gpk::array_pod<byte_t> & output);
 	::gpk::error_t							sessionClientClose							(::gpk::array_pod<byte_t> & output);
+
+	::gpk::error_t							sessionServerClientStart					(::gpk::array_pod<byte_t> & output);
+	::gpk::error_t							sessionServerClientLogin					(::gpk::array_pod<byte_t> & output);
+	::gpk::error_t							sessionServerClientClose					(::gpk::array_pod<byte_t> & output);
 }
 
 #endif // DWH_SESSION_H_2987492834
