13d01d3c624695517a51e07a3aaba2ca90871831
asm128 <pabloarielz@outlook.com>
2018-10-30 08:27:17 -0300

    + Added preliminary code for session transport.

--- dwh/dwh_session.cpp
+++ dwh/dwh_session.cpp
@@ -1,10 +1,135 @@
 #include "dwh_session.h"
+#include "gpk_rsa.h"
 
+static constexpr	const uint32_t				SIZE_MAX_CERTIFICATE										= 2048;
+static constexpr	const uint32_t				SIZE_MAX_KEY												= 256;
+static constexpr	const uint32_t				SIZE_MAX_CLIENT_ID											= 256;
 
-	::gpk::error_t					dwh::sessionClientStart								(::gpk::array_pod<byte_t> & output) { output; return 0; }
-	::gpk::error_t					dwh::sessionClientLogin								(::gpk::array_pod<byte_t> & output) { output; return 0; }
-	::gpk::error_t					dwh::sessionClientClose								(::gpk::array_pod<byte_t> & output) { output; return 0; }
+#pragma pack(push, 1)
+struct SAuthorityClientIdentifyRequest			{
+						byte_t						Certificate			[SIZE_MAX_CERTIFICATE]					= {};
+};
 
-	::gpk::error_t					dwh::sessionServerClientStart						(::gpk::array_pod<byte_t> & output) { output; return 0; }
-	::gpk::error_t					dwh::sessionServerClientLogin						(::gpk::array_pod<byte_t> & output) { output; return 0; }
-	::gpk::error_t					dwh::sessionServerClientClose						(::gpk::array_pod<byte_t> & output) { output; return 0; }
+struct SAuthorityServerIdentifyResponse			{
+						uint64_t					KeysPublic			[SIZE_MAX_KEY]							= {};
+						uint64_t					ClientId			[SIZE_MAX_CLIENT_ID]					= {};
+};
+
+struct SSessionClientStart						{
+						uint64_t					ClientId			[SIZE_MAX_CLIENT_ID]					= {};
+};
+
+struct SAuthorityServiceConfirmClientRequest	{
+						uint64_t					ClientId			[SIZE_MAX_CLIENT_ID]					= {};
+};
+
+struct SAuthorityServerConfirmClientResponse	{
+						uint64_t					KeysPrivate			[SIZE_MAX_KEY]							= {};
+};
+
+struct SSessionServerAccept						{
+						uint64_t					KeysSymmetric		[SIZE_MAX_KEY]							= {};
+};
+
+//struct SSessionClientAccepted					{};
+
+#pragma pack(pop)
+
+// Client sends a request to the authority server and software identifier.																
+					::gpk::error_t				dwh::authorityClientIdentifyRequest					(::dwh::SSessionClient		& client	, ::gpk::array_pod<byte_t> & output)												{ ;   client			, output; 
+	::SAuthorityClientIdentifyRequest					dataToSend											= {};
+
+	// Fill initial request with certificate and client information.
+	for(uint32_t iByte = 0; iByte < ::gpk::size(dataToSend.Certificate); ++iByte)	
+		dataToSend.Certificate[iByte]					= (ubyte_t)(::gpk::size(dataToSend.Certificate) - 1 - iByte);
+
+	gpk_necall(output.resize(sizeof(::SAuthorityClientIdentifyRequest)), "Out of memory?");
+	memcpy(output.begin(), &dataToSend, sizeof(::SAuthorityClientIdentifyRequest));
+	return 0;		 
+}
+
+// Authority's server sends a request with the public key for session startup.															
+					::gpk::error_t				dwh::authorityServerIdentifyResponse				(::dwh::SSessionAuthority	& authority	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output)				{ ;   authority	, input	, output; 
+	const ::SAuthorityClientIdentifyRequest				& dataReceived										= *(::SAuthorityClientIdentifyRequest*)input.begin();
+
+	bool												validCertificate									= true;
+	// Evaluate certificate and client information and send public keys to client.
+	for(uint32_t iByte = 0; iByte < ::gpk::size(dataReceived.Certificate); ++iByte)
+		if(dataReceived.Certificate[iByte] != (ubyte_t)(::gpk::size(dataReceived.Certificate) - 1 - iByte))
+			validCertificate								= false;
+
+	// Evaluate certificate and client information and send public keys to client.
+
+	::SAuthorityServerIdentifyResponse					dataToSend											= {};
+	gpk_necall(output.resize(sizeof(::SAuthorityServerIdentifyResponse)), "Out of memory?");
+	memcpy(output.begin(), &dataToSend, sizeof(::SAuthorityServerIdentifyResponse));
+	return 0; 
+}	
+
+// Client sends a request to the service's server.																						
+					::gpk::error_t				dwh::sessionClientStart								(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output)				{ ;   client	, input	, output; 
+	::SAuthorityServerIdentifyResponse					& dataReceived										= *(::SAuthorityServerIdentifyResponse*)input.begin();
+
+	// Build service connection request encrypted with the public key received from the authority server.
+	dataReceived;
+
+	::SSessionClientStart								dataToSend											= {};
+	gpk_necall(output.resize(sizeof(SSessionClientStart)), "Out of memory?");
+	memcpy(output.begin(), &dataToSend, sizeof(SSessionClientStart));
+	return 0; 
+}	
+
+// Service server sends a request to authority server with client information.															
+					::gpk::error_t				dwh::authorityServiceConfirmClientRequest			(::dwh::SSessionService		& service	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output)				{ ;   service	, input	, output; 
+	::SSessionClientStart								dataReceived										= *(::SSessionClientStart*)input.begin();
+
+	// Build request with client information in order to query the authority server for the private key.
+	dataReceived;
+
+	::SAuthorityServiceConfirmClientRequest				dataToSend											= {};
+	gpk_necall(output.resize(sizeof(SAuthorityServiceConfirmClientRequest)), "Out of memory?");
+	memcpy(output.begin(), &dataToSend, sizeof(SAuthorityServiceConfirmClientRequest));
+	return 0; 
+}	
+
+// Authority server sends a response to service with client information and the private key for decrypting the client password.			
+					::gpk::error_t				dwh::authorityServerConfirmClientResponse			(::dwh::SSessionAuthority	& authority	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output)				{ ;   authority	, input	, output; 
+	::SAuthorityServiceConfirmClientRequest				dataReceived										= *(::SAuthorityServiceConfirmClientRequest*)input.begin();
+
+	// Build response with private key information in order to send to the service and allow it to decrypt the data received from the client.
+	dataReceived;
+
+	::SAuthorityServerConfirmClientResponse				dataToSend											= {};
+	gpk_necall(output.resize(sizeof(SAuthorityServerConfirmClientResponse)), "Out of memory?");
+	memcpy(output.begin(), &dataToSend, sizeof(SAuthorityServerConfirmClientResponse));
+	return 0; 
+}	
+
+// Service server sends a response to the client containing the symmetric keys for the rest of the communication.						
+					::gpk::error_t				dwh::sessionServerAccept							(::dwh::SSessionService		& service	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output)				{ ;   service	, input	, output; 
+	::SAuthorityServerConfirmClientResponse				dataReceived										= *(::SAuthorityServerConfirmClientResponse*)input.begin();
+
+	// Decrypt client data and build accept/reject response to be sent to the client.
+	dataReceived;
+
+	::SSessionServerAccept								dataToSend											= {};
+	gpk_necall(output.resize(sizeof(SSessionServerAccept)), "Out of memory?");
+	memcpy(output.begin(), &dataToSend, sizeof(SSessionServerAccept));
+	return 0; 
+}	
+
+// Client processes service response in order to determine if the connection was accepted as legitimate and loads the symmetric keys.	
+					::gpk::error_t				dwh::sessionClientAccepted							(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output)				{ ;   client	, input	, output; 
+	::SSessionServerAccept								dataReceived										= *(::SSessionServerAccept*)input.begin();
+
+	// Evaluate server response to determine if we can enter into IDLE state or not.
+	dataReceived;
+
+	//::SSessionClientAccepted								dataToSend											= {};
+	//gpk_necall(output.resize(sizeof(SSessionClientAccepted)), "Out of memory?");
+	//memcpy(output.begin(), &dataToSend, sizeof(SSessionClientAccepted));
+	return 0; 
+}	
+
+					::gpk::error_t				dwh::sessionClientEncrypt							(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output) { client, input, output; return 0; }	// 
+					::gpk::error_t				dwh::sessionClientDecrypt							(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output) { client, input, output; return 0; }	// 
--- dwh/dwh_session.h
+++ dwh/dwh_session.h
@@ -5,16 +5,49 @@
 
 namespace dwh
 {
-	struct SSessionClient {};
-	struct SSessionServer { ::gpk::array_pod<SSessionClient> Clients; };
+	enum DWH_SESSION_STAGE 
+		{ DWH_SESSION_STAGE_CLIENT_IDENTIFY						= 0	// authorityClientIdentifyRequest		()
+		, DWH_SESSION_STAGE_AUTHORITY_IDENTIFY						// authorityServerIdentifyResponse		()
+		, DWH_SESSION_STAGE_CLIENT_REQUEST_SERVICE_START			// sessionClientStart					()
+		, DWH_SESSION_STAGE_SERVICE_EVALUATE_CLIENT_REQUEST			// authorityServiceConfirmClientRequest	()
+		, DWH_SESSION_STAGE_AUTHORITY_CONFIRM_CLIENT				// authorityServerConfirmClientResponse	()
+		, DWH_SESSION_STAGE_SERVER_ACCEPT_CLIENT					// sessionServerAccept					()
+		, DWH_SESSION_STAGE_CLIENT_IDLE								// sessionClientAccepted				()
+		};
 
-	::gpk::error_t							sessionClientStart							(::gpk::array_pod<byte_t> & output);
-	::gpk::error_t							sessionClientLogin							(::gpk::array_pod<byte_t> & output);
-	::gpk::error_t							sessionClientClose							(::gpk::array_pod<byte_t> & output);
+	struct SSessionClient		{};
+	struct SSessionClientRecord	{};
+	struct SSessionService		{ ::gpk::array_pod<SSessionClientRecord> Clients; };
+	struct SSessionAuthority	{ ::gpk::array_pod<SSessionClientRecord> Clients; };
 
-	::gpk::error_t							sessionServerClientStart					(::gpk::array_pod<byte_t> & output);
-	::gpk::error_t							sessionServerClientLogin					(::gpk::array_pod<byte_t> & output);
-	::gpk::error_t							sessionServerClientClose					(::gpk::array_pod<byte_t> & output);
+// ----------- Client reports to authority and receives keys and session information - BEGIN
+// Client		-> Authority			// Client sends a request to the authority server and software identifier.																// authorityClientIdentifyRequest		()
+// Authority	-> Client				// Authority's server sends a request with the public key for session startup.															// authorityServerIdentifyResponse		()
+// ----------- Client reports to authority and receives keys and session information - END
+
+// ----------- Client connects to the service - BEGIN
+// Client		-> Service				// Client sends a request to the service's server.																						// sessionClientStart					()
+
+// ----------- Server checks with authority for validity of the client requesting connection - BEGIN
+// Service		-> Authority			// Service server sends a request to authority server with client information.															// authorityServiceConfirmClientRequest	()
+// Authority	-> Service				// Authority server sends a response to service with client information and the private key for decrypting the client password.				// authorityServerConfirmClientResponse	()
+// ----------- Server checks with authority for validity of the client requesting connection - END
+
+// Service		-> Client				// Service server sends a response to the client containing the symmetric keys for the rest of the communication.						// sessionServerAccept					()
+// Client		-> IDLE					// Client processes service response in order to determine if the connection was accepted as legitimate and loads the symmetric keys.	// sessionClientAccepted				()
+// ----------- Client connects to the service - END
+
+	::gpk::error_t							authorityClientIdentifyRequest				(::dwh::SSessionClient		& client	, ::gpk::array_pod<byte_t> & output);										// Client sends a request to the authority server and software identifier.																
+	::gpk::error_t							authorityServerIdentifyResponse				(::dwh::SSessionAuthority	& authority	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Authority's server sends a request with the public key for session startup.															
+	::gpk::error_t							sessionClientStart							(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Client sends a request to the service's server.																						
+	::gpk::error_t							authorityServiceConfirmClientRequest		(::dwh::SSessionService		& service	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Service server sends a request to authority server with client information.															
+	::gpk::error_t							authorityServerConfirmClientResponse		(::dwh::SSessionAuthority	& authority	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Authority server sends a response to service with client information and the private key for decrypting the client password.			
+	::gpk::error_t							sessionServerAccept							(::dwh::SSessionService		& service	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Service server sends a response to the client containing the symmetric keys for the rest of the communication.						
+	::gpk::error_t							sessionClientAccepted						(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Client processes service response in order to determine if the connection was accepted as legitimate and loads the symmetric keys.	
+
+	::gpk::error_t							sessionClientEncrypt						(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Client processes service response in order to determine if the connection was accepted as legitimate and loads the symmetric keys.	
+	::gpk::error_t							sessionClientDecrypt						(::dwh::SSessionClient		& client	, ::gpk::view_array<byte_t> & input, ::gpk::array_pod<byte_t> & output);	// Client processes service response in order to determine if the connection was accepted as legitimate and loads the symmetric keys.	
 }
+
 
 #endif // DWH_SESSION_H_2987492834
